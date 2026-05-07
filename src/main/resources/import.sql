5c88ebb8b92882d7d2ba964de97678aadc24601d
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-05-07 10:33:49 -0600

    Se crea archivo import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO Greeting(id, name)
+VALUES (nextval('Greeting_SEQ'), 'Yesmeli');
+INSERT INTO Greeting(id, name)
+VALUES (nextval('Greeting_SEQ'), 'Yulinet');
+INSERT INTO Greeting(id, name)
+VALUES (nextval('Greeting_SEQ'), 'Madeleine');
